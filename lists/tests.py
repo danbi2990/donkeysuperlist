92e3b33e6e08b98b0c713304c074278b94024e5a
Jake Park <danbi2990@gmail.com>
2017-05-30 19:03:02 +0900

    Three Strikes and Refactor

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
@@ -14,6 +16,24 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
 
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
